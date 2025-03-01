4d988b3b2b0a6a151a92f1529a272f603a4bba07
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-03-01 12:24:18 +0100

    Test with Kubernetes and K3s with CRI-O.

--- tests/run-in-k8s.sh
+++ tests/run-in-k8s.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-set -e
-set -x
-
-if ! [ -f /sys/fs/cgroup/cgroup.controllers ] ; then
-	echo "We expect to only run on cgroups v2 systems." >&2
-	exit 1
-fi
-
-OPTS=
-if [ -e /var/run/docker.sock ] ; then
-	OPTS="$OPTS --docker --kubelet-arg=allowed-unsafe-sysctls=net.ipv6.conf.all.disable_ipv6"
-	patch tests/freeipa-k3s.yaml < tests/freeipa-k3s.yaml.docker.patch
-	patch tests/freeipa-replica-k3s.yaml < tests/freeipa-k3s.yaml.docker.patch
-fi
-mkdir ~/.kube
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig ~/.kube/config $OPTS
-sudo chown $( id -u ):$( id -g ) ~/.kube/config
-( set +x ; while true ; do if kubectl get nodes | tee /dev/stderr | grep -q '\bReady\b' ; then break ; else sleep 5 ; fi ; done )
-kubectl get pods --all-namespaces
-( set +x ; while ! kubectl get serviceaccount/default ; do sleep 5 ; done )
-
-# Make local-path provisioner on userns remapped docker setup on cgroups v2 work
-# -- the pods of the cluster run remapped as well
-sudo mkdir -p /var/lib/rancher/k3s/storage
-sudo chown $( id -u ) /var/lib/rancher/k3s/storage
-
-( set +x ; for i in $( seq 1 10 ) ; do ip link show cni0 > /dev/null && break ; sleep 3 ; done )
-sudo resolvectl dns cni0 $( kubectl get -n kube-system service/kube-dns -o 'jsonpath={.spec.clusterIP}' )
-sudo resolvectl domain cni0 cluster.local
-
-kubectl apply -f tests/freeipa-coredns-custom.yaml
-
 kubectl create -f <( sed "s#image:.*#image: $1#" tests/freeipa-k3s.yaml )
 ( set +x ; while kubectl get pod/freeipa-server | tee /dev/stderr | grep -Eq '\bPending\b|\bContainerCreating\b' ; do sleep 5 ; done )
 if ! kubectl get pod/freeipa-server | grep -q '\bRunning\b' ; then
@@ -47,7 +15,7 @@ trap "kill $MASTER_LOGS_PID 2> /dev/null || : ; trap - EXIT" EXIT
 kubectl describe pod/freeipa-server
 kubectl exec freeipa-server -- cat /proc/1/uid_map | tee /dev/stderr | grep -q '^ *0 *[1-9]'
 PV_DIR=$( kubectl get pvc/freeipa-data-pvc -o 'jsonpath={.spec.volumeName}_{.metadata.namespace}_{.metadata.name}' )
-ls -la /var/lib/rancher/k3s/storage/$PV_DIR
+ls -la {/opt/local-path-provisioner,/var/lib/rancher/k3s/storage}/$PV_DIR
 IPA_SERVER_HOSTNAME=$( kubectl exec pod/freeipa-server -- hostname -f )
 IPA_SERVER_IP=$( kubectl get -o=jsonpath='{.spec.clusterIP}' service freeipa-server-service )
 seq 15 -1 0 | while read i ; do dig +short $IPA_SERVER_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_SERVER_IP && break ; sleep 5 ; [ $i == 0 ] && false ; done
@@ -75,7 +43,7 @@ trap "kill $REPLICA_LOGS_PID 2> /dev/null || : ; trap - EXIT" EXIT
 kubectl describe pod/freeipa-replica
 kubectl exec freeipa-replica -- cat /proc/1/uid_map | tee /dev/stderr | grep -q '^ *0 *[1-9]'
 PV_DIR=$( kubectl get pvc/freeipa-replica-pvc -o 'jsonpath={.spec.volumeName}_{.metadata.namespace}_{.metadata.name}' )
-ls -la /var/lib/rancher/k3s/storage/$PV_DIR
+ls -la {/opt/local-path-provisioner,/var/lib/rancher/k3s/storage}/$PV_DIR
 IPA_REPLICA_HOSTNAME=$( kubectl exec pod/freeipa-replica -- hostname -f )
 IPA_REPLICA_IP=$( kubectl get -o=jsonpath='{.spec.clusterIP}' service freeipa-replica-service )
 seq 15 -1 0 | while read i ; do dig +short $IPA_REPLICA_HOSTNAME | tee /dev/stderr | grep -Fq $IPA_REPLICA_IP && break ; sleep 5 ; [ $i == 0 ] && false ; done
